e649e5f5d850b678b9ab09313b8436bf1359d9c1
pankajkushw <pankaj.kushw@gmail.com>
2026-03-10 03:13:15 +0530

    added nav bar login link  with {% url 'login_app:login' %}

--- kgm/kgm/urls.py
+++ kgm/kgm/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('home.urls')),
     path('login/',include('login_app.urls')),
+    path('',include('login_app.urls')),
     path('admin/', admin.site.urls),
 
 ]
--- kgm/login_app/urls.py
+++ kgm/login_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from login_app import views
 
+app_name = 'login_app'
+
 urlpatterns = [
     path('', views.login, name='login'),
 ]
