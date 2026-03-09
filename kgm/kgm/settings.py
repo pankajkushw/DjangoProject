777e2c55bc261acda610811e5b3b9120eef12649
pankajkushw <pankaj.kushw@gmail.com>
2026-03-09 13:09:30 +0530

    Fixed Static issue in kgm project. Adding Carousel

--- kgm/kgm/settings.py
+++ kgm/kgm/settings.py
@@ -15,7 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = BASE_DIR / "templates"
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',   # e.g., project-level static folder
+]
 
 
 # Quick-start development settings - unsuitable for production
